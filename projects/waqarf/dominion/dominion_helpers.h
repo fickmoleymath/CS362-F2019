029442d2f89c5f89c5852b403864f823e79c6eaf
Faaiq Waqar <waqarf@flip1.engr.oregonstate.edu>
2019-10-13 15:11:40 -0700

    completed functions and bug introductions

--- projects/waqarf/dominion/dominion_helpers.h
+++ projects/waqarf/dominion/dominion_helpers.h
@@ -12,4 +12,13 @@ int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3,
                struct gameState *state, int handPos, int *bonus);
 
+int baronCardEffect(int baronChoice, struct gameState * state, int currentPlayer);
+int minionCardEffect(int handPos, int currentPlayer, int minionChoiceOne,
+                int minionChoiceTwo, struct gameState *state);
+int ambassadorCardEffect(int ambassadorChoiceOne, int ambassadorChoiceTwo,
+                int currentPlayer, int handPos, struct gameState *state);
+int tributeCardEffect(int nextPlayer, int currentPlayer, struct gameState *state);
+int mineCardEffect(int mineChoiceOne, int mineChoiceTwo, int currentPlayer,
+                int handPos, struct gameState *state);
+
 #endif
